a3adffe5deecbb579ef19fa887a8c3312c853c36
Marco Cognolato <marco.cognolato.98@gmail.com>
2020-09-30 18:09:35 +0200

    Added basic logger implementation + testing ground
    feat: added custom py logger with basic colors implementation
    feat: added testing ground to test stuff

---
+++ tester.py
@@ -0,0 +1,13 @@
+from logger import *
+
+# print(REVERSED + "Hola" + RESET)
+# print(BOLD + "Hola")
+# print(UNDERLINE + "Hola" + RESET)
+
+LOG_ERROR("An Error Occurred Unexpectedly")
+LOG_WARNING("Something strange is happening")
+LOG_SUCCESS("Everything is working fine")
+LOG_INFO("Something is working...")
+
+#print(RESET + BACK_BLUE + "Some Text")
+print(reset)
