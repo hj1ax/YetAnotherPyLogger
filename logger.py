c9769c1f2302966828628e75a4fd89f50ea82645
Cogno-Marco <marco.cognolato.98@gmail.com>
2020-09-30 18:56:43 +0200

    Tested new API + improved old one
    test: tested new API
    style: improved old API

--- logger.py
+++ logger.py
@@ -4,39 +4,42 @@ os.system("")
 
 reset = "\u001b[0m"
 
-bold =      "\33[1m"
-italic =    "\33[3m"
-url =       "\33[4m"
-blink =     "\33[5m"
-underline = "\u001b[4m"
-reversed =  "\u001b[7m"
+class Styling:
+    bold =      "\33[1m"
+    italic =    "\33[3m"
+    url =       "\33[4m"
+    blink =     "\33[5m"
+    underline = "\u001b[4m"
+    reversed =  "\u001b[7m"
 
-black =     "\u001b[38;5;0m"
-gray =      "\u001b[38;5;8m"
-red =       "\u001b[38;5;9m"
-green =     "\u001b[38;5;10m"
-yellow =    "\u001b[38;5;11m"
-blue =      "\u001b[38;5;12m"
-purple =    "\u001b[38;5;13m"
-cyan =      "\u001b[38;5;14m"
-white =     "\u001b[38;5;15m"
+class Color:
+    black =     "\u001b[38;5;0m"
+    gray =      "\u001b[38;5;8m"
+    red =       "\u001b[38;5;9m"
+    green =     "\u001b[38;5;10m"
+    yellow =    "\u001b[38;5;11m"
+    blue =      "\u001b[38;5;12m"
+    purple =    "\u001b[38;5;13m"
+    cyan =      "\u001b[38;5;14m"
+    white =     "\u001b[38;5;15m"
 
-bg_black =  "\u001b[48;5;0m"
-bg_gray =   "\u001b[48;5;8m"
-bg_red =    "\u001b[48;5;9m"
-bg_green =  "\u001b[48;5;10m"
-bg_yellow = "\u001b[48;5;11m"
-bg_blue =   "\u001b[48;5;12m"
-bg_purple = "\u001b[48;5;13m"
-bg_cyan =   "\u001b[48;5;14m"
-bg_white =  "\u001b[48;5;15m"
+class BGColor:
+    bg_black =  "\u001b[48;5;0m"
+    bg_gray =   "\u001b[48;5;8m"
+    bg_red =    "\u001b[48;5;9m"
+    bg_green =  "\u001b[48;5;10m"
+    bg_yellow = "\u001b[48;5;11m"
+    bg_blue =   "\u001b[48;5;12m"
+    bg_purple = "\u001b[48;5;13m"
+    bg_cyan =   "\u001b[48;5;14m"
+    bg_white =  "\u001b[48;5;15m"
 
 
-def LOG_ERROR(text):    print(reset + red    + bold + "[-] Error: "   + reset + text + reset)
-def LOG_WARNING(text):  print(reset + yellow + bold + "[!] Warning: " + reset + text + reset)
-def LOG_INFO(text):     print(reset + blue   + bold + "[?] Info: "    + reset + text + reset)
-def LOG_SUCCESS(text):  print(reset + green  + bold + "[+] Success: " + reset + text + reset)
+def ERROR(text):    print(reset + Color.red    + Styling.bold + "[-] Error: "   + reset + text + reset)
+def WARNING(text):  print(reset + Color.yellow + Styling.bold + "[!] Warning: " + reset + text + reset)
+def INFO(text):     print(reset + Color.blue   + Styling.bold + "[?] Info: "    + reset + text + reset)
+def SUCCESS(text):  print(reset + Color.green  + Styling.bold + "[+] Success: " + reset + text + reset)
 
-def CUSTOM_COLOR(number):    print(f"\u001b[38;5;{number}m")
-def CUSTOM_BG_COLOR(number): print(f"\u001b[48;5;{number}m")
+def setCustomColor(number):   print(f"\u001b[38;5;{number}m")
+def setCustomBGColor(number): print(f"\u001b[48;5;{number}m")
 
--- tester.py
+++ tester.py
@@ -1,13 +1,28 @@
-from logger import *
+import logger as Log
 
 # print(REVERSED + "Hola" + RESET)
 # print(BOLD + "Hola")
 # print(UNDERLINE + "Hola" + RESET)
 
-LOG_ERROR("An Error Occurred Unexpectedly")
-LOG_WARNING("Something strange is happening")
-LOG_SUCCESS("Everything is working fine")
-LOG_INFO("Something is working...")
+Log.ERROR("An Error Occurred Unexpectedly")
+Log.WARNING("Something strange is happening")
+Log.SUCCESS("Everything is working fine")
+Log.INFO("Something is working...")
 
 #print(RESET + BACK_BLUE + "Some Text")
-print(reset)
+print(Log.reset)
+
+class Logger:
+    pass
+
+log = Log.LoggerBuilder()
+log.startPattern()
+log.setColor(Log.Colors.RED).setStyle(Log.Styles.BOLD)
+log.setText("[-] Error: ")
+log.resetStyle()
+log.setInputText()
+log.resetStyle()
+log.endPattern()
+log.setPublic()
+
+
